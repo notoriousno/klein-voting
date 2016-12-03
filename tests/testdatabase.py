dddc430ca0577576c9634c5f194a40ddced7a1cc
dev <notorious.no@gmail.com>
2016-12-03 14:24:56 -0500

    add Database, Votes tests

--- tests/testdatabase.py
+++ tests/testdatabase.py
@@ -7,11 +7,12 @@ filepath = path.split(path.split(path.realpath(__file__))[0])[0]
 sys.path.append(filepath)   # add project directory to Python path
 
 try:
-    from unittest.mock import MagicMock
+    from unittest.mock import MagicMock, patch
 except ImportError:
-    from mock import MagicMock
+    from mock import MagicMock, patch
 from itertools import chain
-from twisted.internet.defer import gatherResults
+from twisted.enterprise.adbapi import ConnectionPool
+from twisted.internet.defer import gatherResults, inlineCallbacks
 from twisted.trial.unittest import TestCase
 from zope.interface.verify import verifyClass
 from database import Database, Candidates, Validations, Votes
@@ -57,7 +58,59 @@ class TestValidations(TestCase):
             self.assertRaises(AssertionError, self.validate.validate_candidate_name, name)
 
 class TestDatabase(TestCase):
-    pass
+
+    @patch('twisted.enterprise.adbapi.ConnectionPool')
+    def test_mocked_database(self, ConnectionPool):
+        dbpool = ConnectionPool('db_module', 'db_uri', *(), **{})
+        db = Database(dbpool)
+
+        # query
+        query_stmt = 'select * from sometable where something=1'
+        db.execute(query_stmt)
+        dbpool.runQuery.assert_called_with(query_stmt)
+
+        # other SQL statements
+        sql_stmt = 'insert into sometable (col0, col1, col2) values (val0, val1, val2)'
+        db.execute(sql_stmt)
+        dbpool.runInteraction.assert_called_with(db._execute, sql_stmt)
+
+    @inlineCallbacks
+    def test_real_database(self):
+        from os import path
+        import sqlite3
+
+        db_name = 'UNITTEST.sqlite'
+        db_path = path.realpath('UNITTEST.sqlite')
+
+        dbpool = ConnectionPool('sqlite3', db_path, check_same_thread=False)
+        db = Database(dbpool)
+        table_name = 'test_table'
+
+        # check if the db exists, create it if it doesn't
+        if not path.exists(db_path):
+            create_table_stmt = 'create table %s (key int primary key, name text)' % (table_name)
+            yield db.execute(create_table_stmt)
+
+        # remove any unnecessary files after this test case runs
+        self.addCleanup(self.remove_test_files, db_path)
+
+        # insert a record into the table
+        key = 1
+        name = 'test'
+        insert_stmt = "insert into %s (key, name) values (%d, '%s')" % (table_name, key, name)
+        yield db.execute(insert_stmt)
+
+        # use the DBAPI 2.0 module to verify results
+        connection = sqlite3.connect(db_name)
+        cursor = connection.cursor()
+        query_stmt = 'select key, name from %s where key=%d' % (table_name, key)
+        cursor.execute(query_stmt)
+        assert cursor.fetchone() == (key, name)
+
+    def remove_test_files(self, *file_paths):
+        from os import remove
+        for file_path in file_paths:
+            remove(file_path)
 
 class TestCandidates(TestCase):
 
@@ -106,7 +159,7 @@ class TestCandidates(TestCase):
 
         @d.addCallback
         def unexpected_success(result):
-            raise Exception()
+            raise Exception('Unexpected success')
 
         @d.addErrback
         def verify_failure(failure):
@@ -184,5 +237,60 @@ class TestCandidates(TestCase):
             self.assertRaises(AssertionError, self.candidates.add_candidate, item)
 
 class TestVotes(TestCase):
+
+    table_name = 'votes'
+
+    def setUp(self):
+        self.db = MagicMock()
+        self.candidates = MagicMock()
+        self.candidates.table_name = 'candidates'
+        self.votes = Votes(self.db, self.candidates)
+
     def test_contract(self):
         assert verifyClass(IVotes, Votes)
+
+    def test_create_table(self):
+        self.votes.create_table()
+        sql_stmt = 'create table %s (candidate int primary key, votes int not null, foreign key(candidate) references %s(id))' % \
+            (self.table_name, self.candidates.table_name)
+        self.votes.db.execute.assert_called_with(sql_stmt)
+
+    def test_vote_for_candidate_exist_in_table(self):
+        candidate_id = 1
+        votes = 100
+        record = (candidate_id, 'Candidate Name', votes)
+        # mock a method call, devs MUST keep the expected results up-to-date!
+        self.votes.candidate_record = MagicMock(return_value = [record])
+
+        d = self.votes.vote_for(candidate_id)
+        @d.addCallback
+        def verify_update(results):
+            sql_stmt = "update %s set votes=%d where candidate=%d" % (self.table_name, votes+1, candidate_id)
+            self.db.execute.assert_called_with(sql_stmt)
+
+    def test_vote_for_candidate_first_time(self):
+        candidate_id = 1
+        record = (candidate_id, 'Candidate Name')
+        # mock a method call, devs MUST keep the expected results up-to-date!
+        self.candidates.get_candidate_by_id.return_value = [record]
+
+        d = self.votes.vote_for(candidate_id)
+        @d.addCallback
+        def verify_insert(results):
+            sql_stmt = "insert into %s (candidate, votes) values (%d, 1)" % (self.table_name, candidate_id)
+            self.db.execute.assert_called_with(sql_stmt)
+
+    def test_vote_for_candidate_not_exist(self):
+        self.votes.candidate_record = MagicMock(return_value = [])
+        self.candidates.get_candidate_by_id.return_value = []
+
+        d = self.votes.vote_for(1000000)
+        @d.addCallback
+        def unexpected_success(result):
+            raise Exception('Unexpected success')
+
+        @d.addErrback
+        def verify_exception(failure):
+            exception = failure.value
+            assert isinstance(exception, IndexError)
+            assert str(exception) == 'Candidate id is not present.'
